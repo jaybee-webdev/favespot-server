01fd60e8d26b55f64bbc4f736272b860dc5cb654
jaybee-webdev <jaybee.webdev@gmail.com>
2021-02-18 12:57:02 +0800

    test changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,22 @@
-module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASS,
-    DB: process.env.DB_NAME,
+// module.exports = {
+//     HOST: process.env.DB_HOST,
+//     USER: process.env.DB_USER,
+//     PASSWORD: process.env.DB_PASS,
+//     DB: process.env.DB_NAME,
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
+  module.exports = {
+    HOST: process.env.HRK_DB_HOST,
+    USER: process.env.HRK_DB_USER,
+    PASSWORD: process.env.HRK_DB_PASS,
+    DB: process.env.HRK_DB_NAME,
     dialect: "mysql",
     pool: {
       max: 5,
@@ -11,17 +25,3 @@ module.exports = {
       idle: 10000
     }
   };
-
-  // module.exports = {
-  //   HOST: process.env.HRK_DB_HOST,
-  //   USER: process.env.HRK_DB_USER,
-  //   PASSWORD: process.env.HRK_DB_PASS,
-  //   DB: process.env.HRK_DB_NAME,
-  //   dialect: "mysql",
-  //   pool: {
-  //     max: 5,
-  //     min: 0,
-  //     acquire: 30000,
-  //     idle: 10000
-  //   }
-  // };
